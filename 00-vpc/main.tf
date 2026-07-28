21b0d82ad6837bc840cdf9d76f48bd8225d4f25d
swathi <battulaswathi15407@gmail.com>
2026-07-28 13:59:09 +0530

    jenkins

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/swathibattula-B/terraform-vpc.git?ref=main"
+    source = "git::https://github.com/daws-88s/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
